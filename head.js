1479a0f1721363cc54833e7f9d3188198a240ab8
Abyan Mohamed <abyanmohamed99@gmail.com>
2021-11-23 01:58:38 +0000

    Completed head.js

---
+++ head.js
@@ -0,0 +1,13 @@
+let assertEqual = function(value1, value2) {
+    if (value1 === value2) {
+      console.log(`✅✅✅ Assertion Passed: ${value1} === ${value2}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${value1} !== ${value2}`);
+    }
+  };
+    // Prints the first element in the array
+  let head = function (array){
+
+  }
+
+  assertEqual(head[3,5,7], 9)
