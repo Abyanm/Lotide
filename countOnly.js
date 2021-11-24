b2e9b8992d3eb21b3abbecbe3c872455df8bc07a
Abyan Mohamed <abyanmohamed99@gmail.com>
2021-11-24 23:46:41 +0000

    Completed CountOnly.js

---
+++ countOnly.js
@@ -0,0 +1,43 @@
+let assertEqual = function(value1, value2) {
+  if (value1 === value2) {
+      console.log(`✅✅✅ Assertion Passed: ${value1} === ${value2}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${value1} !== ${value2}`);
+    }
+  };
+
+
+  // allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) { 
+    const results = {}
+       for (const item of allItems) {
+        if (itemsToCount[item]) {
+        
+        if (results[item]) {
+            results[item] += 1;
+          } else {
+            results[item] = 1;
+          }}
+      }
+
+   return results;
+}
+const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  
+  const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  //console.log(result1)
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
