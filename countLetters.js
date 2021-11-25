db8f9f5adfc8f1bfe6bf6899841d0225fc69c7cc
Abyan Mohamed <abyanmohamed99@gmail.com>
2021-11-25 00:29:10 +0000

    CompletedLetters.js

---
+++ countLetters.js
@@ -0,0 +1,16 @@
+let assertEqual = function(value1, value2) {
+    if (value1 === value2) {
+        console.log(`✅✅✅ Assertion Passed: ${value1} === ${value2}`);
+      } else {
+        console.log(`🛑🛑🛑 Assertion Failed: ${value1} !== ${value2}`);
+      }
+    };
+const countLetters = function(letters){
+    const result = {}
+    for (letter of letters) {    // assigns the letters of the word to the variable.
+        console.log(letter)
+   //     if(letters[words) {
+     //   }
+    }
+}  
+countLetters("Home")
